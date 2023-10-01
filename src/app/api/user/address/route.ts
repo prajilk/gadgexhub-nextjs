5559e8a2d1bfdccaabb5747cda3d787d94f0f2b5
Prajil K <prajilk20017@gmail.com>
2023-10-01 16:26:15 +0530

    added cookie to the fetch request

--- src/app/api/user/address/route.ts
+++ src/app/api/user/address/route.ts
@@ -6,7 +6,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions);
-  console.log(session);
 
   if (!session || !session.user || !session.user.id) {
     return NextResponse.json(
