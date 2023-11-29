d0cfc4cb42526b73b6981f1e68311feacb1a05b3
Prajil K <prajilk20017@gmail.com>
2023-11-29 16:32:10 +0530

    refactored footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,8 +4,9 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="mt-auto w-full bg-black text-white">
-      <div className="mx-auto flex max-w-6xl flex-col-reverse justify-between gap-7 px-4 py-10 sm:flex-row">
-        <div className="ms-3 flex flex-col">
+      {/* <div className="mx-auto flex max-w-6xl flex-col-reverse justify-between gap-7 px-4 py-10 sm:flex-row"> */}
+      <div className="mx-auto grid max-w-6xl grid-cols-1 gap-7 px-4 py-10 sm:grid-cols-2">
+        <div className="order-2 ms-3 flex flex-col">
           <h1 className="mb-3 text-lg font-bold sm:text-xl">GadgeXhub</h1>
           <p className="mb-10 text-xs text-[#828282]">
             &copy; 2023 - {new Date().getFullYear()} GadgeXhub. All Rights
@@ -26,7 +27,7 @@ const Footer = () => {
             </Link>
           </div>
         </div>
-        <div className="flex flex-1 justify-around sm:justify-evenly">
+        <div className="ms-3 grid grid-cols-2 sm:order-3">
           <div>
             <h2 className="text-sm">Store</h2>
             <ul className="mt-5 text-xs text-[#828282]">
@@ -50,6 +51,30 @@ const Footer = () => {
             </ul>
           </div>
         </div>
+        {/* <div className="flex flex-1 justify-around sm:justify-evenly">
+          <div>
+            <h2 className="text-sm">Store</h2>
+            <ul className="mt-5 text-xs text-[#828282]">
+              <li className="my-3 w-fit duration-200 hover:text-white">
+                <Link href="/store/c/accessories">Accessories</Link>
+              </li>
+              <li className="my-3 w-fit duration-200 hover:text-white">
+                <Link href="/store/c/gadgets">Gadgets</Link>
+              </li>
+              <li className="my-3 w-fit duration-200 hover:text-white">
+                <Link href="/store/c/audio-video">Audio/Video</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-sm">Support</h2>
+            <ul className="mt-5 text-xs text-[#828282]">
+              <li className="my-3 w-fit duration-200 hover:text-white">
+                <Link href="/contact-us">Contact us</Link>
+              </li>
+            </ul>
+          </div>
+        </div> */}
       </div>
     </footer>
   );
